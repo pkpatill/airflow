193cd4fdc15295b0cf35944bf877c41dbad77266
Pranav <31706902+pkpatill@users.noreply.github.com>
2024-08-03 20:16:48 +0530

    Update test_dag.py

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -11,7 +11,7 @@ default_args={
    'email': ['patilpk15@gmail.com'],
    'email_on_failure': False,
    'email_on_retry': False,
-   'retries': 1,
+   'retries': 0,
    'retry_delay': timedelta(minutes=5)
 }
 with DAG(
