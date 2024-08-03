853620fa63045f57b6ac5023c2a82e6fc67481f5
Pranav <31706902+pkpatill@users.noreply.github.com>
2024-08-04 03:06:12 +0530

    Create new.py

---
+++ dags/new.py
@@ -0,0 +1,34 @@
+from datetime import timedelta
+import os
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.utils.dates import days_ago
+from airflow.models import Variable
+
+default_args = {
+    'owner': 'Howdy',
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'pi',
+    start_date=days_ago(1),
+    catchup=False,
+    schedule_interval=timedelta(days=1),
+    #template_searchpath='/opt/airflow/dags/repo/dags/spark8s/'
+)
+
+spark_k8s_task = SparkKubernetesOperator(
+    task_id='n-spark-on-k8s-airflow',
+    trigger_rule="all_success",
+    depends_on_past=False,
+    retries=0,
+    application_file='pi.yaml',
+    namespace="spark-jobs",
+    kubernetes_conn_id="kubernetes_default",
+    do_xcom_push=False,
+    dag=dag
+)
+
+spark_k8s_task
